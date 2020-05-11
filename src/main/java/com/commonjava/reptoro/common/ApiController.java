c75fe3858bc045af7f9b5d3a9400f27e674dd023
Gorgi Georgievski <ggeorgie@redhat.com>
2020-05-11 10:08:14 +0200

    Jenkins Pipeline build

--- src/main/java/com/commonjava/reptoro/common/ApiController.java
+++ src/main/java/com/commonjava/reptoro/common/ApiController.java
@@ -15,6 +15,7 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.SocketAddress;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.oauth2.KeycloakHelper;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
@@ -649,20 +650,33 @@ public class ApiController extends AbstractVerticle {
   }
 
   public void currentuser(RoutingContext context) {
-    String accessToken = KeycloakHelper.rawAccessToken(context.user().principal());
-    JsonObject token = KeycloakHelper.parseToken(accessToken);
+    @Nullable User ctxUser = context.user();
 
-    OAuth2TokenImpl user = (OAuth2TokenImpl) context.user();
-    context.setUser(user);
+    if(Objects.nonNull(ctxUser)) {
+      String accessToken = KeycloakHelper.rawAccessToken(context.user().principal());
+      JsonObject token = KeycloakHelper.parseToken(accessToken);
+
+      OAuth2TokenImpl user = (OAuth2TokenImpl) context.user();
+      context.setUser(user);
 
 
-    context.response()
-      .putHeader(CONTENT_TYPE,APPLICATION_JSON_CHARSET_UTF_8)
-      .end(new JsonObject()
-        .put("id",token.getString("jti"))
-        .put("token",token)
-        .put("username",token.getString("preferred_username"))
-        .encodePrettily());
+      context.response()
+          .putHeader(CONTENT_TYPE,APPLICATION_JSON_CHARSET_UTF_8)
+          .end(new JsonObject()
+              .put("id",token.getString("jti"))
+              .put("token",token)
+              .put("username",token.getString("preferred_username"))
+              .encodePrettily());
+    } else {
+      context.response()
+          .putHeader(CONTENT_TYPE,APPLICATION_JSON_CHARSET_UTF_8)
+          .end(new JsonObject()
+              .put("id","none")
+              .put("token","none")
+              .put("username","none")
+              .encodePrettily());
+    }
+
   }
 
   public void handleGetAllRepositories(RoutingContext rc) {
